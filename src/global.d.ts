5b6532ad0348639804f4683a26742b5205abbf72
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-03-31 17:12:03 +0200

    up deps

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,9 +2,6 @@ declare global {
   declare namespace Route {
     export type Endpoint = '/api/posts/:id' | '/api/posts' | '/'
   }
-  declare namespace Environment {
-    export type Key = 
-  }
 }
 
 export {};
