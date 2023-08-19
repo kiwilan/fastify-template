5760bb0e97b0a343668d0e53b958179acb847b0f
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-08-19 10:51:07 +0200

    lint

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,6 @@
 import { Router } from '@kiwilan/fastify-utils'
 
-const metaRoutes = () => {
+function metaRoutes() {
   return {
     home: Router.route('/'),
     posts: Router.route('/api/posts'),
